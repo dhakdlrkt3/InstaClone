f15a8572e49fa70c66232d79ff83305c0bbacaa1
hutsu.yoo <hutsu.yoo@dinosband.com>
2021-07-01 21:43:04 +0900

    main state

--- App.tsx
+++ App.tsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import { SafeAreaView, StyleSheet } from 'react-native'
-import MainNavigator from './src/screens/MainNavigator'
 
 
 export default function App(): JSX.Element {
 	return (
 		<SafeAreaView style={[styles.safeAreaView]}>
-			<MainNavigator />
 		</SafeAreaView>
 	)
 }
