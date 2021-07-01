d3934cd991376b3f8a83ad69397b80ca2231c966
hutsu.yoo <hutsu.yoo@dinosband.com>
2021-07-01 22:23:27 +0900

    set navigator

--- src/copy/MainNavigator.tsx
+++ src/copy/MainNavigator.tsx
@@ -1,13 +1,14 @@
 import React, { useState } from 'react'
 import { BottomNavigation } from 'react-native-paper'
+// import Home from './Home'
 
 export default function MainNavigator(): JSX.Element {
 	const [index, setIndex] = useState<number>(0)
 	const [routes] = useState([
-
+		{ key: 'home', title: 'Home', icons: 'home' }
 	])
 	const renderScene = BottomNavigation.SceneMap({
-
+		//home: Home
 	})
 	return (
 		<BottomNavigation
--- src/screens/MainNavigator.tsx
+++ src/screens/MainNavigator.tsx
@@ -1,13 +1,23 @@
 import React, { useState } from 'react'
 import { BottomNavigation } from 'react-native-paper'
+import LifeCycle from './LifeCycle'
+import Timer from './Timer'
+import Interval from './Interval'
+import Fetch from './Fetch'
 
 export default function MainNavigator(): JSX.Element {
 	const [index, setIndex] = useState<number>(0)
 	const [routes] = useState([
-
+		{ key: 'life', title: 'LifeCycle', icons: 'page-layout-header-footer' },
+		{ key: 'timer', title: 'Timer', icons: 'clock-timer-four' },
+		{ key: 'interval', title: 'Interval', icons: 'timerline' },
+		{ key: 'fetch', title: 'Fetch', icons: 'history' }
 	])
 	const renderScene = BottomNavigation.SceneMap({
-
+		life: LifeCycle,
+		timer: Timer,
+		Interval: Interval,
+		fetch: Fetch,
 	})
 	return (
 		<BottomNavigation
