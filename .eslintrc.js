95c1739c5df5ad2ba4b3a69d5a68a45fb6ab2506
hutsu.yoo <hutsu.yoo@dinosband.com>
2021-07-01 21:41:28 +0900

    default setting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,27 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
-};
+	env: {
+		es2021: true,
+		node: true,
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:@typescript-eslint/recommended',
+		// "@react-native-community",
+	],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 12,
+		sourceType: 'module',
+	},
+	plugins: ['react', '@typescript-eslint'],
+	rules: {
+		'object-curly-spacing': ['warn', 'always'],
+		indent: ['error', 'tab'],
+		quotes: ['error', 'single'],
+		semi: ['error', 'never'],
+	},
+}
